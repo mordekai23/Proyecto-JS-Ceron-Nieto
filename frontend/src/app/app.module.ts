15bd8d0da3848c9333417ee6bc7315302b4d6fab
mordekai23 <danilo48_its@yahoo.com>
2020-02-17 17:58:44 -0500

    ruta login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RutaLoginComponent } from './rutas/ruta-login/ruta-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RutaLoginComponent
   ],
   imports: [
     BrowserModule,
