ea9116601168ca0197371a56cad78d4c9fbb1090
mordekai23 <danilo48_its@yahoo.com>
2020-02-25 22:40:26 -0500

    revert commit

---
+++ frontend/src/app/services/auth/politicas/es_usuario.policy.ts
@@ -0,0 +1,33 @@
+import {Injectable} from "@angular/core";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
+import {AuthServices} from "../authService";
+
+@Injectable({
+  providedIn:'root'
+})
+
+export class  EsUsuarioPolicy implements CanActivate{
+
+  constructor(
+    private readonly _authService:AuthServices,
+    private router:Router,
+  )
+  {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean
+    {
+      if(this._authService.sesion.rol.toLowerCase() ==='administrador')
+      {
+        console.log('No tiene permiso');
+        this.router.navigate(['/inicio']);
+        return true;
+      }
+      else {
+        return false;
+      }
+
+  }
+
+
+
+}
