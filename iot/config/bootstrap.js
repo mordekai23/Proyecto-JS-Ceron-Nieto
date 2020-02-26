e93a5268bb7e1fd3ede96fad5b088f326d9ff993
mordekai23 <danilo48_its@yahoo.com>
2020-02-25 22:32:40 -0500

    update iot

--- iot/config/bootstrap.js
+++ iot/config/bootstrap.js
@@ -61,7 +61,6 @@ module.exports.bootstrap =  function(done) {
     proximity.on("change", async () => {
       var sleep = require('sleep');
       const {centimeters, inches} = proximity;
-
       if(centimeters < 30){
         console.log("Proximity: ");
         console.log("  cm  : ", centimeters);
